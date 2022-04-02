c49850a0e83a97ec221695683988cd9d2eeb4247
Alcides <acremonezi@gmail.com>
2022-04-02 02:32:02 -0300

    MySQL Setup

--- devops/django_gunicorn/entrypoint.sh
+++ devops/django_gunicorn/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+python manage.py makemigrations --no-input
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 
--- django_project/config/settings.py
+++ django_project/config/settings.py
@@ -74,13 +74,24 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_project',
+        'USER': 'django_project',
+        'PASSWORD': 'i2H9F9ukEGi2H9F9ukEGVZWNpY4lrHVZWNpY4lrH',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
