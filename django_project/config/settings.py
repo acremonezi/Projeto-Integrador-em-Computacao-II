c14ba295542c0aa270e32738ad7ad4ae30aa3bf8
Alcides <acremonezi@gmail.com>
2022-04-02 13:04:20 -0300

    start

--- django_project/config/settings.py
+++ django_project/config/settings.py
@@ -76,10 +76,21 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_project',
+        'USER': 'django_project',
+        'PASSWORD': 'i2H9F9ukEGi2H9F9ukEGVZWNpY4lrHVZWNpY4lrH',
+        'HOST': 'mysql',
+        'PORT': '3306',
     }
 }
 
