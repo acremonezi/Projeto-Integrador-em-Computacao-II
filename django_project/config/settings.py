6ca95196bbd63f2aa8abdafa6bec82a78dafef19
Alcides <acremonezi@gmail.com>
2022-04-02 00:45:02 -0300

    Update settings.py

--- django_project/config/settings.py
+++ django_project/config/settings.py
@@ -121,8 +121,6 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
 STATIC_ROOT = os.path.join('static')
 
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
