f2b5a5ef2360c6fa89557229569212b43497f537
Alcides <acremonezi@gmail.com>
2022-05-21 23:26:54 -0300

    closes #26 Added Main into the admin panel

--- django_project/main/admin.py
+++ django_project/main/admin.py
@@ -1,3 +1,11 @@
+from re import A
 from django.contrib import admin
+from main.models import Account, Project, Objective, Metric
 
 # Register your models here.
+
+# Simple Method
+admin.site.register(Account)
+admin.site.register(Project)
+admin.site.register(Objective)
+admin.site.register(Metric)
