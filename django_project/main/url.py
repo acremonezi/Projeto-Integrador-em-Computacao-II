ab707750d1db90f96ad4ab1ae3d60a37d150fc3c
Alcides <acremonezi@gmail.com>
2022-05-29 13:27:20 -0300

    #33 Created URLs for

--- django_project/main/url.py
+++ django_project/main/url.py
@@ -5,4 +5,7 @@ app_name = 'main'
 
 urlpatterns = [
     path('create-account/', views.createAccount, name = "Create Account"),
+    path('create-project/', views.createProject, name = "Create Project"),
+    path('create-objective/', views.createObjective, name = "Create Objective"),
+    path('create-metric/', views.createMetric, name = "Create Metric"),
 ]
