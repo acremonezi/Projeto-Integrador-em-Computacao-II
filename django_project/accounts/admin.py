c6160d23da5abacffceb25f61cfc43af8f47914f
2005869 <2005869@aluno.univesp.br>
2022-06-28 00:03:26 -0300

    Closes #56, user profile added to project

--- django_project/accounts/admin.py
+++ django_project/accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+@admin.register(models.UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'photo']
+
--- django_project/accounts/models.py
+++ django_project/accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.conf import settings
+
 
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to='users/%Y/%m/%d/', blank=True)
+
+    def __str__(self):
+        return f'Profile for user {self.user.username}'
+
+    class Meta:
+        verbose_name = 'User Profile'
+        verbose_name_plural = 'Users Profile'
